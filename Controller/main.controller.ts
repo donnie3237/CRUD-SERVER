d941d6d07c698686a7e0297ea6becefaf963f8b7
pardorn3237 <paradorn3237@gmail.com>
2023-02-03 19:35:04 +0700

    .

--- Controller/main.controller.ts
+++ Controller/main.controller.ts
@@ -2,45 +2,41 @@
 import express from 'express';
 const { ObjectID , ObjectId} = require('bson');
 const users = require('../model/main.model.ts')
-type action = {
-    req:express.Request,
-    res:express.Response
-}
 
-exports.getdata =(req:action,res:action)=>{
+exports.getdata =(req:express.Request,res:express.Response)=>{
     const User = users
     User.find({}, function(err:any, users:any) {
-        if (err) {res.res.send("err")}
-        res.res.send(users)
+        if (err) {res.send("error")}
+        res.send(users)
     });
 }
 
-exports.getOne =(req:action,res:action)=>{
+exports.getOne =(req:express.Request,res:express.Response)=>{
     const User = users
-    User.find({_id : ObjectId(req.req.params['id'])}, function(err:any, users:any) {
-        if (err) {res.res.send("err")}
-        res.res.send(users)
+    User.find({_id : ObjectId(req.params['id'])}, function(err:any, users:any) {
+        if (err) {res.send("err")}
+        res.send(users)
     });
 }
 
-exports.delete = (req:action,res:action)=>{
+exports.delete = (req:express.Request,res:express.Response)=>{
     const User = users
-    User.deleteOne({_id : ObjectId(req.req.params['id'])}, function (err:any) {
+    User.deleteOne({_id : ObjectId(req.params['id'])}, function (err:any) {
         if(err){console.log("error")}
         console.log("finished")
       });
 }
 
-exports.update = (req:action,res:action)=>{
+exports.update = (req:express.Request,res:express.Response)=>{
     const User = users
-    const { name, age, height, weight, desc } = req.req.body;
-    User.updateOne({_id : ObjectId(req.req.params['id'])},
+    const { name, age, height, weight, desc } = req.body;
+    User.updateOne({_id : ObjectId(req.params['id'])},
         {$set : { name, age, height, weight, desc }}
-    ).then(res.res.send("updated"));
+    ).then(res.send("updated"));
 }
 
-exports.add = (req:action,res:action) => {
-    const { name, age, height, weight, desc } = req.req.body;
+exports.add = (req:express.Request,res:express.Response) => {
+    const { name, age, height, weight, desc } = req.body;
     const new_user = new users({
         name,age,height,weight,desc
     })
@@ -49,7 +45,7 @@ exports.add = (req:action,res:action) => {
             console.log(err);
         } else {
             console.log(result)
-            res.res.send("hello")
+            res.send("hello")
         }
     })
 };  
--- Routes/main.route.ts
+++ Routes/main.route.ts
@@ -2,30 +2,26 @@ import express from 'express';
 const Main_router : any = express.Router();
 const path = require('path');
 const controller = require('../Controller/main.controller.ts');
-type Action = {
-    req: express.Request;
-    res: express.Response;
-};
 
 Main_router.use(express.json());
 Main_router.use(express.urlencoded({ extended: true }));
 
-Main_router.get('/',(req : Action,res : Action)=>{
+Main_router.get('/',(req :express.Request,res : express.Response)=>{
     controller.getdata(req,res);
 })
-Main_router.post('/add',(req : Action,res : Action)=>{
+Main_router.post('/add',(req :express.Request,res : express.Response)=>{
     controller.add(req,res);
 })
-Main_router.get('/api',(req : Action,res : Action)=>{
+Main_router.get('/api',(req :express.Request,res : express.Response)=>{
     controller.getdata(req,res);
 })
-Main_router.get('/see/:id',(req : Action,res : Action)=>{
+Main_router.get('/see/:id',(req :express.Request,res : express.Response)=>{
     controller.getOne(req,res);
 })
-Main_router.delete('/delete/:id',(req : Action,res : Action)=>{
+Main_router.delete('/delete/:id',(req :express.Request,res : express.Response)=>{
     controller.delete(req,res);
 })
-Main_router.put('/update/:id',(req : Action,res : Action)=>{
+Main_router.put('/update/:id',(req :express.Request,res : express.Response)=>{
     controller.update(req,res);
 })
 
