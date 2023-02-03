67cc676b75c873ac4a0acc17e90b1a44da38139a
pardorn3237 <paradorn3237@gmail.com>
2023-02-03 22:55:17 +0700

    .

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('./DB/db')
-const express = require('express') 
+import express from 'express';
 const app = express();
 const PORT:string | number | undefined = process.env.PORT;
 import cors from 'cors'
@@ -11,6 +11,11 @@ app.use("/",Main_router)
 // middle ware
 app.use(express.json())
 app.use(cors());
+app.use(function(req : express.Request, res:express.Response, next :express.NextFunction) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({ extended : true}))
 
 //listan port
