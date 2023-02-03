5e19972dd4b514a784c2895ca1021b7f918241ca
pardorn3237 <paradorn3237@gmail.com>
2023-02-03 12:56:15 +0700

    ..

--- Routes/main.route.ts
+++ Routes/main.route.ts
@@ -2,7 +2,6 @@ import express from 'express';
 const Main_router : any = express.Router();
 const path = require('path');
 const controller = require('../Controller/main.controller.js');
-
 type Action = {
     req: express.Request;
     res: express.Response;
@@ -12,11 +11,11 @@ Main_router.use(express.json());
 Main_router.use(express.urlencoded({ extended: true }));
 
 Main_router.get("/", (req : Action,res : Action)=>{
-    res.res.sendFile(path.join(__dirname,'./server.html'))
+    res.res.send("hello")
 })
-Main_router.get('/code',(req : Action,res : Action)=>{
-    res.res.sendFile(path.join(__dirname,'../index.js'))
-})
+// Main_router.get('/code',(req : Action,res : Action)=>{
+//     res.res.sendFile(path.join(__dirname,'../index.js'))
+// })
 Main_router.post('/add',(req : Action,res : Action)=>{
     controller.add(req,res);
 })
